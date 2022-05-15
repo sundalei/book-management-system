aa0046f4a9693b6005124c25e766a71eb703e0e8
INFOR\dsun1 <dalei.sun@infor.com>
2022-05-15 17:14:22 +0800

    repository definition

--- src/main/java/com/example/ui/domain/Book.java
+++ src/main/java/com/example/ui/domain/Book.java
@@ -2,12 +2,19 @@ package com.example.ui.domain;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Data;
 
 @Data
+@Document("books")
 public class Book {
 
+    @Id
     private String id;
+
+    private String isbn;
 
     private String title;
 
